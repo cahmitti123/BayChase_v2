773b24205f7fd808e6335b3a0135836032d4862e
cahmitti123 <ahmitti.chouaib@gmail.com>
2024-10-20 21:02:14 +0100

    packages prices changed

--- constants/content.ts
+++ constants/content.ts
@@ -1,113 +1,118 @@
-
 export const FEATURES = [
-    {
-        feature:"Meal",
-        price:0,
-        description:"This package includes one daily meal (lunch)."
-    },
-    {
-        feature:"Equipment",
-        price:0,
-        description:"Boards and WetSuits"
-    },
-    {
-        feature:"Photographer",
-        price:50,
-        description:"A dedicated photographer will accompany us to capture the memorable moments of the trip."
-    },
-]
+  {
+    feature: "Meal",
+    price: 0,
+    description: "This package includes one daily meal (lunch).",
+  },
+  {
+    feature: "Equipment",
+    price: 0,
+    description: "Boards and WetSuits",
+  },
+  {
+    feature: "Photographer",
+    price: 50,
+    description:
+      "A dedicated photographer will accompany us to capture the memorable moments of the trip.",
+  },
+];
 
 export const PACKAGES = [
-    {
-        label:"Surf Guiding",
-        price:110,
-        href:'/packages',
-        img: '/images/surf_guiding.jpeg',
-        unit:"Day",
-        type:"Guiding",
-        description:"The Surf Guiding package provides you with the best and unparalleled local knowledge to access world-class,uncrowded waves. This package is designed to optimize your surfing experience by ensuring that you have access to the highest quality waves suitable for your skill level. This implies that you will consistently find yourself in the optimal location for your skill level, riding the finest waves available that align with your proficiency.",
-        features:[
-            {
-                feature:"Meal",
-                price:'Included',
-                description:"This package includes one daily meal (lunch)."
-            },
-            {
-                feature:"Equipment",
-                price:'Included',
-                description:"Boards and WetSuits"
-            },
-            {
-                feature:"Photographer",
-                price:'50  €',
-                description:"A dedicated photographer will accompany us to capture the memorable moments of the trip."
-            }
-
-        ]
-    },
-    {
-        label:"Beginners Package",
-        price:40,
-        href:'/packages',
-        img: '/images/beginners_package.jpeg',
-        unit:"Session",
-        type:"Teaching",
-        description:"For beginners, acquire foundational surfing skills while gaining an understanding of ocean dynamics and tide theory.",
-        features:[
-            {
-                feature:"Equipment",
-                price:'Included',
-                description:"Boards and WetSuits"
-            },
-            {
-                feature:"Photographer",
-                price:"30  €",
-                description:"A dedicated photographer will accompany us to capture the memorable moments of the trip."
-            }
-
-        ]
-    },
-    {
-        label:"Intermediate Package",
-        price:55,
-        href:'/packages',
-        img: '/images/intermediate_lessons.jpeg',
-        unit:"Session",
-        type:"Teaching",
-        description:"For intermediate surfers, improve your advanced maneuvers and your surfing style.",
-        features:[
-            {
-                feature:"Equipment",
-                price:'Included',
-                description:"Boards and WetSuits"
-            },
-            {
-                feature:"Photographer",
-                price:'30  €',
-                description:"A dedicated photographer will accompany us to capture the memorable moments of the trip."
-            }
-
-        ]
-    },
-    {
-        label:"Private lesson",
-        price:70,
-        href:'/packages',
-        img: '/images/parivate_lessons.jpeg',
-        unit:"Session",
-        type:"Teaching",
-        description:"If you're eager to progress rapidly, consider our private lesson option. This personalized session allows me to focus exclusively on you, addressing mistakes and accelerating your improvement. ",
-        features:[
-            {
-                feature:"Equipment",
-                price:'Included',
-                description:"Boards and WetSuits"
-            },
-            {
-                feature:"Photographer",
-                price:'70  €',
-                description:"A dedicated photographer will accompany us to capture the memorable moments of the trip."
-            }
-        ]
-    },
-]
+  {
+    label: "Surf Guiding",
+    price: 880,
+    href: "/packages",
+    img: "/images/surf_guiding.jpeg",
+    unit: "7 Days",
+    type: "Guiding",
+    description:
+      "The Surf Guiding package provides you with the best and unparalleled local knowledge to access world-class,uncrowded waves. This package is designed to optimize your surfing experience by ensuring that you have access to the highest quality waves suitable for your skill level. This implies that you will consistently find yourself in the optimal location for your skill level, riding the finest waves available that align with your proficiency.",
+    features: [
+      {
+        feature: "Meal",
+        price: "Included",
+        description: "This package includes one daily meal (lunch).",
+      },
+      {
+        feature: "Equipment",
+        price: "Included",
+        description: "Boards and WetSuits",
+      },
+      {
+        feature: "Photographer",
+        price: "50  €",
+        description:
+          "A dedicated photographer will accompany us to capture the memorable moments of the trip.",
+      },
+    ],
+  },
+  {
+    label: "Beginners Package",
+    price: 660,
+    href: "/packages",
+    img: "/images/beginners_package.jpeg",
+    unit: "6 Days",
+    type: "Teaching",
+    description:
+      "For beginners, acquire foundational surfing skills while gaining an understanding of ocean dynamics and tide theory.",
+    features: [
+      {
+        feature: "Equipment",
+        price: "Included",
+        description: "Boards and WetSuits",
+      },
+      {
+        feature: "Photographer",
+        price: "30  €",
+        description:
+          "A dedicated photographer will accompany us to capture the memorable moments of the trip.",
+      },
+    ],
+  },
+  {
+    label: "Intermediate Package",
+    price: 750,
+    href: "/packages",
+    img: "/images/intermediate_lessons.jpeg",
+    unit: "7 Days",
+    type: "Teaching",
+    description:
+      "For intermediate surfers, improve your advanced maneuvers and your surfing style.",
+    features: [
+      {
+        feature: "Equipment",
+        price: "Included",
+        description: "Boards and WetSuits",
+      },
+      {
+        feature: "Photographer",
+        price: "30  €",
+        description:
+          "A dedicated photographer will accompany us to capture the memorable moments of the trip.",
+      },
+    ],
+  },
+  {
+    label: "Private lesson",
+    price: 70,
+    href: "/packages",
+    img: "/images/parivate_lessons.jpeg",
+    unit: "Session",
+    type: "Teaching",
+    description:
+      "If you're eager to progress rapidly, consider our private lesson option. This personalized session allows me to focus exclusively on you, addressing mistakes and accelerating your improvement. ",
+    features: [
+      {
+        feature: "Equipment",
+        price: "Included",
+        description: "Boards and WetSuits",
+      },
+      {
+        feature: "Photographer",
+        price: "70  €",
+        description:
+          "A dedicated photographer will accompany us to capture the memorable moments of the trip.",
+      },
+    ],
+  },
+];
